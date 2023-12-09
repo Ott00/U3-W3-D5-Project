4c0bcc6d20b3c66adc67785e5c43ae691e3caceb
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-12-09 14:44:05 +0100

    added profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  userInfo!: User;
+  constructor() {}
 
   ngOnInit(): void {
+    this.getUserInfo();
+    console.log(this.userInfo);
   }
 
+  getUserInfo() {
+    const userString: any = localStorage.getItem('user');
+    const user = JSON.parse(userString).user;
+    this.userInfo = user;
+  }
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,6 @@
 export interface User {
-  name: string;
-  surname: string;
+  nome: string;
+  cognome: string;
   email: string;
   id: string;
 }
